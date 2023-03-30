4cb756eb743d629afff33b4f69576138b76b229a
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-30 13:13:51 -0500

    implementando provider for new adapter axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeAPI } from './interface/Poke-api.interface';
 
@@ -10,16 +10,17 @@ export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
-  private readonly axios: AxiosInstance = axios;
-
   async executeSeed() {
-    const { data } = await this.axios.get<PokeAPI>(
+    const data = await this.http.get<PokeAPI>(
       'https://pokeapi.co/api/v2/pokemon?limit=300',
     );
     await this.pokemonModel.deleteMany();
+
     const InserManyNewData: Array<{ name: string; no: number }> = [];
+
     data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
